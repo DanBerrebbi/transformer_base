fd51df03913589a99117ee1eab89442d0e90eaef
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-21 19:36:46 +0200

    folders

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -1,7 +1,7 @@
 
 
 #dnet=$PWD/model_serie
-dnet=$PWD/model_checkpoint
+dnet=$PWD/model_base_dan
 josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 data=$josep/data
 tokenizer=tools/tokenizer.py
@@ -9,7 +9,5 @@ fref=$data/clean.Europarl.en-fr.fr.tst
 
 # sort -g model_serie/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
 
-sort -g Europarl_josep | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
+sort -g model_base_dan/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_base_dan/Europarl.out_k5_alpha0.7.bleu
 
-sort -g Europarl_josep > test.la
-sort -g model_serie/Europarl.out_k5_alpha0.7 > test.dan
