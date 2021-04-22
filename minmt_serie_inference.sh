65766e982a34c9a2ef16d1c2ab9e9e559209d43c
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 12:08:04 +0200

    inference double batch 10000

--- minmt_serie_inference.sh
+++ minmt_serie_inference.sh
@@ -14,10 +14,10 @@ data=$josep/data
 stovec=$josep/stovec
 
 dir=$josep/minmt_base
-dnet=$PWD/model_base_dan
+dnet=$PWD/model_serie_double_batch
 
 
-fmod=$dnet/network.checkpoint_00280000.pt ####### A MODIF
+fmod=$dnet/network.checkpoint_00010000.pt ####### A MODIF
 
 	for corpus in Europarl; do
 	    echo $corpus
